fd050e493d1a9e602b774892b14c6f209883e7a1
Muhammad Islam <islamjutt56.i@gmail.com>
2026-07-21 08:57:56 +0500

    Set up Express server with book routes

--- task2/app.js
+++ task2/app.js
@@ -1,1 +1,39 @@
 
+const express = require('express');
+const path = require('path');
+const { router: bookRoutes } = require('./routes/books');
+
+const app = express();
+const requestedPort = Number(process.env.PORT || 3000);
+
+app.use(express.json());
+app.use('/api/books', bookRoutes);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+function startServer(port) {
+  const server = app.listen(port, '0.0.0.0');
+
+  server.on('listening', () => {
+    console.log(`Server running on http://localhost:${port}`);
+  });
+
+  server.on('error', (error) => {
+    if (error.code === 'EADDRINUSE') {
+      const fallbackPort = port + 1;
+      console.log(`Port ${port} is busy, trying ${fallbackPort}...`);
+      startServer(fallbackPort);
+      return;
+    }
+
+    throw error;
+  });
+}
+
+if (require.main === module) {
+  startServer(requestedPort);
+}
+
+module.exports = { app, startServer };
